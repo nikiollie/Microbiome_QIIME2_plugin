6916ae05c86bca04bf10a4b96d7c2d099fb9e541
brandonespino <bespinoh@ucsd.edu>
2019-02-04 21:56:30 -0800

    one hot encoder takes in fasta

---
+++ MetaGAN/Genomes/one_hot.py
@@ -0,0 +1,22 @@
+import numpy as np
+def read_fasta(fp):
+        name, seq = None, []
+        for line in fp:
+            line = line.rstrip()
+            if line.startswith(">"):
+                if name: yield (name, ''.join(seq))
+                name, seq = line, []
+            else:
+                seq.append(line)
+        if name: yield (name, ''.join(seq))
+def vectorizeSequence(seq):
+    # the order of the letters is not arbitrary.
+    # Flip the matrix up-down and left-right for reverse compliment
+    ltrdict = {'A':[1,0,0,0],'C':[0,1,0,0],'G':[0,0,1,0],'T':[0,0,0,1]}
+    return np.array([ltrdict[x] for x in seq])
+
+with open('GCA_000005845.2_ASM584v2_genomic.fna') as fp:
+    for name, seq in read_fasta(fp):
+        i = 0
+    array = vectorizeSequence(seq)
+    print((array)) 
