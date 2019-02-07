9166171aa80846b86d40045580a44c73ca23c244
Chris <mrchrisrobles@gmail.com>
2019-02-07 08:48:36 -0800

    Edited dataParser.py so it returns a string of genomic values when called

--- dataParser.py
+++ dataParser.py
@@ -1,46 +1,45 @@
 import sys
 import csv
 import os
-
+import skbio
 
 def dataParser(input_file):
-    base = input_file
     #remove '.fna' from file name string
-    name_file = os.path.splitext(base)[0]
+    name_file = os.path.splitext(input_file)[0]
     
-    #create a new file and write to it(modify file)
-    f = open('MetaGAN/Edited_Genomes/' + name_file + ".txt", "w")
-
-    #open file and read to it(display)
-    with open('MetaGAN/Genomes/' + name_file + ".fna", "r") as fin:
-        data = fin.read().splitlines(True)
+    sequences = list(skbio.io.read('MetaGAN/Genomes/' + input_file, 
+        format='fasta')) 
+    
+    sequence = ""
     #open file and write to it(modify file)
-    with open('MetaGAN/Edited_Genomes/' + name_file + ".txt", 'w') as fout:
-        #remove first line from file
-        fout.writelines(data[1:]) 
+    #with open('MetaGAN/Edited_Genomes/Edited_' + name_file + ".fna", 'w') as fout:
+    for value in sequences[0].values:
+        sequence += str(value.decode('UTF-8')) 
+    
+    return sequence
 
 #state the directory where the genomes are located
-directory_in_str = 'MetaGAN\Genomes'
+#directory_in_str = 'MetaGAN/Genomes'
 
 #encode path-like filename to the filesystem encoding
 #return bytes unchanged
-directory = os.fsencode(directory_in_str)
+#directory = os.fsencode(directory_in_str)
 
 #listdir function returns a list containing the names 
 #of the entries in the directory given by the path
 #iterate through every file in the directory
-for file in os.listdir(directory):
+#for file in os.listdir(directory):
     
     #fsdecode function decodes the path-like filename 
     #from the filesystem encoding and returns the str unchanged 
-    filename = os.fsdecode(file)
-    if filename.endswith(".fna"): 
+    #filename = os.fsdecode(file)
+    #if filename.endswith(".fna"): 
         # print(os.path.join(directory, filename))
         #call dataParser function 
-        dataParser(filename)
-        continue
-    else:
-        continue
+        #dataParser(filename)
+        #continue
+    #else:
+        #continue
 
 
 
