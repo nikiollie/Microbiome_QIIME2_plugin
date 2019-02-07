c17d33de7896e22714f4afac8966567117a2c762
brandonespino <bespinoh@ucsd.edu>
2019-02-06 22:42:46 -0800

    one_hot2.py is an edited version of one_hot.py from Monday

---
+++ MetaGAN/Genomes/one_hot2.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def read_fasta(fp):
+        name, seq = None, []
+        for line in fp:
+            line = line.rstrip()
+            if line.startswith(">"):
+                if name: yield (name, ''.join(seq))
+                name, seq = line, []
+            else:
+                seq.append(line)
+        if name: yield (name, ''.join(seq))
+
+def vectorizeSequence(seq):
+    # the order of the letters is not arbitrary.
+    # Flip the matrix up-down and left-right for reverse compliment
+    ltrdict = {'A':[1,0,0,0],'C':[0,1,0,0],'G':[0,0,1,0],'T':[0,0,0,1]}
+    return np.array([ltrdict[x] for x in seq])
+
+#accepts input file from console (.fna)
+fileInput = input("enter file: ")
+with open(str(fileInput)) as fp:
+    for name, seq in read_fasta(fp):
+        i = 0
+    #one hot encoded DNA sequence
+    one_hot_seq = vectorizeSequence(seq)
+    
+#length of the DNA sample
+seqLength = len(one_hot_seq)
+#list to store the 10000 150x4 training examples
+training_examples = []
+for i in range(0,10000):
+    #generates 150 random integers to parse the DNA sample
+    indices  = np.random.randint(seqLength,size = 150)
+    #a single 150x4 random example
+    example = one_hot_seq[indices,:]
+    training_examples.append(example)
+
+#accepts input label from console
+labels = input("enter label: ")
+#array of 10000 labels of input name to attach to the training 10000x2 matrix 
+labels = np.asarray([str(labels)]*10000)
+training_examples = np.asarray(training_examples)
+print(labels.shape)
+print(training_examples.shape)
+print(training_examples)
+#labeled_examples = np.vstack((training_examples,labels))
+#training_examples = np.asarray(labeled_examples_list)
