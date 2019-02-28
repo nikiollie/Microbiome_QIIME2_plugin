a5a6a0be912e996de816c492ca814c1bcdc0e49a
Raechel Walker <r6walker@ucsd.edu>
2019-02-28 10:06:25 -0800

    new pickle

--- MetaGAN/Genomes/one_hot2.py
+++ MetaGAN/Genomes/one_hot2.py
@@ -3,6 +3,7 @@ import os
 import csv
 import sys
 import skbio
+import pickle
 def read_fasta(fp):
         name, seq = None, []
         for line in fp:
@@ -24,38 +25,37 @@ def vectorizeSequence(seq):
     #return [[ltrdict[x] for x in seq]]
 #accepts input file from console (.fna)
 #fileInput = input("enter file: ")
-directory1 ='~/Microbiome_QIIME2_plugin/MetaGAN/Genomes'
+directory1 = os.getcwd()
 
 directory = os.fsencode(directory1)
 for file in os.listdir(directory):
     filename = os.fsdecode(file)
-"""
-    print("type of file: "+str(type(file)))
-    #str1 = ''.join(str(x) for x in file)
-    #file = open(str(file)+'.csv', 'w')
-    #file.write(str1) 
-    #file.close()
-#with open(str(fileInput)) as fp:
-    #for name, seq in read_fasta(fp):
-        #i = 0
-    #one hot encoded DNA sequence
-    #one_hot_seq = vectorizeSequence(seq)
-    
-#length of the DNA sample
+    if filename.endswith(".fna"):
+        with open(filename) as fp:
+            #print(filename)
+            for name, seq in read_fasta(fp):
+                #print(filename)
+                i = 0
+            one_hot_seq = vectorizeSequence(seq)
+        #length of the DNA sample
+        seqLength = len(one_hot_seq)
+        #list to store the 10000 150x4 training examples
+        training_examples = []
+        for i in range(0,10000):
+            #generates 150 random integers to parse the DNA sample
+            indices  = np.random.randint(seqLength,size = 150)
+    	    #a single 150x4 random example
+            example = one_hot_seq[indices,:]
+            training_examples.append(example)
 
-seqLength = len(one_hot_seq)
-#list to store the 10000 150x4 training examples
-training_examples = []
-for i in range(0,10000):
-    #generates 150 random integers to parse the DNA sample
-    indices  = np.random.randint(seqLength,size = 150)
-    #a single 150x4 random example
-    example = one_hot_seq[indices,:]
-    training_examples.append(example)
+        #accepts input label from console
+        labels = input("enter label: ")
+        #create an array with 10K elements for 10K rows
+        labels = np.asarray([str(labels)]*10000)
+        training_examples = np.asarray(training_examples)
+    else:
+        continue
+    file1 = open(str(file)+'.pickle', 'wb')
+    pickle.dump(training_examples,file1)
+    file1.close()
 
-#accepts input label from console
-labels = input("enter label: ")
-#create an array with 10K elements for 10K rows
-labels = np.asarray([str(labels)]*10000)
-training_examples = np.asarray(training_examples)
-"""
